fef692eae1c10a040a1a52f351b448497ec2ec22
Nastya Vertal <veeertal@yandex.ru>
2022-05-27 22:04:16 +0300

    deploy test

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось и я почти все понимаю!')
 
 
 def second_page(request):
